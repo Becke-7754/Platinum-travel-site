c4b67374eb35fd0514ccd70f46e3ddae97992130
Becke <bilguun.dama@yahoo.com>
2020-08-21 19:42:12 +0800

    Anhni Cloud Function hiilee (edited)

--- app/cloud-functions/secret-page.js
+++ app/cloud-functions/secret-page.js
@@ -1,4 +1,4 @@
-exports / handle = function (events, context, callback) {
+exports.handle = function (events, context, callback) {
   callback(null, {
     statuscode: 200,
     body: "Хэрэглэгчдийн хуудсанд тавтай морилно уу",
